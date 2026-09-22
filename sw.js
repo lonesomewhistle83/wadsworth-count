eee32b4f1cf742fbcf1debdb2d679ec93a7da5d5
lonesomewhistle83 <lonesomewhistle83@gmail.com>
2026-09-22 11:47:07 -0400

    Wadsworth count app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='wadsworth-count-r10';
+var CACHE='wadsworth-count-r11';
 var SHELL=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png','favicon-32.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(SHELL);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
